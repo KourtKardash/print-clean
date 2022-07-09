316eb25aea5ec88fc8a7a97bd7b5150d7d434c93
Danylo Mysak <danmysak@gmail.com>
2022-07-09 18:19:09 +0300

    Script & readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # print-clean
-Cleans photographed documents for improved printing
+
+This small script cleans photographed documents for improved printing:
+
+Before:
+
+![before](/demo/demo.jpg)
+
+After:
+
+![after](/demo/demo-cleaned.png)
+
+Usage:
+
+```
+python clean.py path-to-image
+```
+
+The output image will be saved as a PNG file with the suffix `-cleaned`.
---
+++ clean.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+from math import copysign
+from pathlib import Path
+
+from numpy import uint8, vectorize
+from skimage.filters import threshold_local
+from skimage.io import imread, imsave
+
+BLOCK_SIZE = 99
+OFFSET = 10 / 255
+STRENGTH_THRESHOLD = 0.02
+OUTPUT_SUFFIX = '-cleaned'
+OUTPUT_EXTENSION = 'png'
+
+
+def compute_strength(diff: float) -> float:
+    strength = min(abs(diff), STRENGTH_THRESHOLD) * 0.5 / STRENGTH_THRESHOLD
+    return 0.5 + copysign(strength, diff)
+
+
+def get_output_path(input_path: Path) -> Path:
+    return input_path.parent / f'{input_path.stem}{OUTPUT_SUFFIX}.{OUTPUT_EXTENSION}'
+
+
+args = ArgumentParser()
+args.add_argument('image', help='Path to the image file')
+path = Path(args.parse_args().image)
+
+if not path.exists():
+    raise Exception(f'{path} does not exist')
+
+if not path.is_file():
+    raise Exception(f'{path} is not a file')
+
+image = imread(path, as_gray=True)
+threshold = threshold_local(image, BLOCK_SIZE, offset=OFFSET)
+imsave(get_output_path(path), (vectorize(compute_strength)(image - threshold) * 255).astype(uint8))
