2559a11a37144a02f7cb3108ab6ad8a063849fe9
Danylo Mysak <danmysak@gmail.com>
2022-07-14 22:39:19 +0300

    Perspective/rotations

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # print-clean
 
-This small script cleans photographed documents for improved printing:
+This script helps prepare photographed documents for printing:
 
 Before:
 
@@ -13,7 +13,11 @@ After:
 Usage:
 
 ```
-python clean.py path-to-image
+python clean.py path-to-image [--lang code1 code2 ...]
 ```
+
+You may provide your document’s language(s) with the `--lang` parameter, which may help fix the photo’s perspective. Currently, the script can only perform simple rotations.
+
+You can find available languages and their codes [here](https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html). You might need to specifically install your required language for the Tesseract OCR engine ([Windows](https://stackoverflow.com/a/69958671/430083), [Ubuntu](https://askubuntu.com/a/798492/1064838), [Mac OS](https://stackoverflow.com/a/60595075/430083)).
 
 The output image will be saved as a PNG file with the suffix `-cleaned`.
--- clean.py
+++ clean.py
@@ -1,11 +1,16 @@
 from argparse import ArgumentParser
 from math import copysign
 from pathlib import Path
+from sys import exit
+from typing import Optional
 
-from numpy import uint8, vectorize
+from numpy import vectorize
 from skimage.filters import threshold_local
 from skimage.io import imread, imsave
 from skimage.restoration import denoise_tv_chambolle
+
+from inc.perspective import fix_perspective
+from inc.utils import float_to_uint8
 
 BLOCK_SIZE = 99
 OFFSET = 10 / 255
@@ -13,6 +18,25 @@ DENOISE_WEIGHT = 0.03
 STRENGTH_THRESHOLD = 0.02
 OUTPUT_SUFFIX = '-cleaned'
 OUTPUT_EXTENSION = 'png'
+
+args = ArgumentParser()
+args.add_argument('image', help='Path to the image file.')
+args.add_argument(
+    '--lang',
+    nargs='+',
+    help='Language(s) of the document. This is used to fix perspective of the photo. '
+         'Use language codes from https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html.',
+)
+args = args.parse_args()
+
+path = Path(args.image)
+languages: Optional[list[str]] = args.lang
+
+if not path.exists():
+    exit(f'{path} does not exist')
+
+if not path.is_file():
+    exit(f'{path} is not a file')
 
 
 def compute_strength(diff: float) -> float:
@@ -24,17 +48,13 @@ def get_output_path(input_path: Path) -> Path:
     return input_path.parent / f'{input_path.stem}{OUTPUT_SUFFIX}.{OUTPUT_EXTENSION}'
 
 
-args = ArgumentParser()
-args.add_argument('image', help='Path to the image file')
-path = Path(args.parse_args().image)
-
-if not path.exists():
-    raise Exception(f'{path} does not exist')
-
-if not path.is_file():
-    raise Exception(f'{path} is not a file')
-
 image = imread(path, as_gray=True)
 image = denoise_tv_chambolle(image, weight=DENOISE_WEIGHT)
 threshold = threshold_local(image, BLOCK_SIZE, offset=OFFSET)
-imsave(get_output_path(path), (vectorize(compute_strength)(image - threshold) * 255).astype(uint8))
+image = vectorize(compute_strength)(image - threshold)
+if languages:
+    try:
+        image = fix_perspective(image, languages)
+    except ValueError as error:
+        exit(error.args)
+imsave(get_output_path(path), float_to_uint8(image))
