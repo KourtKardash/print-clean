0a638ddf7d5778f5eeceb91459718e216ca1c0c2
Краснова Светлана <svetlana.krasnova@inside.glowbyte.com>
2024-11-12 15:22:09 +0300

    Added another way to remove shadows

--- src/printclean/clean.py
+++ src/printclean/clean.py
@@ -3,27 +3,20 @@ from math import copysign
 from pathlib import Path
 from sys import exit
 from typing import Optional
-
-from numpy import vectorize
-from skimage.filters import threshold_local
-from skimage.io import imread, imsave
-from skimage.restoration import denoise_tv_chambolle
+import cv2
+import numpy as np
 
 from .perspective import fix_perspective
-from .utils import float_to_uint8
 
-BLOCK_SIZE = 99
-DEFAULT_LEVEL = 10
-MAX_LEVEL = 255
+BLURRED_WND = 21
 DENOISE_WEIGHT = 0.03
-STRENGTH_THRESHOLD = 0.02
 OUTPUT_SUFFIX = '-cleaned'
 OUTPUT_EXTENSION = 'png'
 
-
-def compute_strength(diff: float) -> float:
-    strength = min(abs(diff), STRENGTH_THRESHOLD) * 0.5 / STRENGTH_THRESHOLD
-    return 0.5 + copysign(strength, diff)
+def adjust_gamma(image, gamma=1.0):
+    inv_gamma = 1.0 / gamma
+    table = np.array([(i / 255.0) ** inv_gamma * 255 for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
 
 
 def get_output_path(input_path: Path) -> Path:
@@ -34,13 +27,6 @@ def run() -> None:
     args = ArgumentParser()
     args.add_argument('images', nargs='+', help='Path to the image file(s).')
     args.add_argument(
-        '--level',
-        nargs='?',
-        default=DEFAULT_LEVEL,
-        type=int,
-        help=f'The cleanup threshold, a value between 0 and {MAX_LEVEL} (larger is more aggressive).',
-    )
-    args.add_argument(
         '--lang',
         nargs='+',
         help='Language(s) of the document. This is used to fix perspective of the photo. '
@@ -50,7 +36,6 @@ def run() -> None:
 
     paths = list(map(Path, args.images))
     languages: Optional[list[str]] = args.lang
-    level: int = args.level
 
     for path in paths:
         if not path.exists():
@@ -58,20 +43,20 @@ def run() -> None:
         if not path.is_file():
             exit(f'{path} is not a file')
 
-    if not 0 <= level <= MAX_LEVEL:
-        exit(f'Level is not between 0 and {MAX_LEVEL}')
-
     for index, path in enumerate(paths):
         print(f'Processing {path} ({index + 1} of {len(paths)})...')
-        image = imread(path, as_gray=True)
-        image = denoise_tv_chambolle(image, weight=DENOISE_WEIGHT)
-        threshold = threshold_local(image, BLOCK_SIZE, offset=level / MAX_LEVEL)
-        image = vectorize(compute_strength)(image - threshold)
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(image, (BLURRED_WND, BLURRED_WND), 0)
+        shadow_removed = cv2.divide(image, blurred, scale=255)
+        image = adjust_gamma(shadow_removed , gamma=0.3)
+
         if languages:
             try:
                 image = fix_perspective(image, languages)
             except ValueError as error:
                 exit(error.args)
-        imsave(get_output_path(path), float_to_uint8(image))
-
+        cv2.imwrite(get_output_path(path), image)
     print('Done')
+
+run()
